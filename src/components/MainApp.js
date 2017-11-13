28d624620bcaf311e0163ab401a19925f212965a
Michał Balcerzak <mb385130@students.mimuw.edu.pl>
2017-11-13 20:08:06 +0100

    Init npm and Gatsby build system

---
+++ src/components/MainApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+      <div>
+        There was
+        a HOLE
+            here.
+          <div />
+          Its gone now.
+          <div />
+          <div />
+        
+        Tutaj niebawem będą się pojawiać rzeczy związane z najbliższym projektem zaliczeniowym związanym z przedmiotem Bazy Danych.
+      </div>
+  );
+};
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Main from '../components/MainApp';
+ 
+export default () => (<Main/>);
+
