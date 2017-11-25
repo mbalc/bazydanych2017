2e147bcd485076ad275db9a520dc5dc00373d366
mbalc <mbalc909@gmail.com>
2017-11-25 12:02:20 +0100

    Fix deps, syntax, filenames

---
+++ src/components/MainApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    There was
+    a HOLE
+        here.
+    <div />
+      Its gone now.
+    <div />
+    <div />
+      Tutaj niebawem będą się pojawiać rzeczy związane z najbliższym
+      projektem zaliczeniowym związanym z przedmiotem Bazy Danych.
+  </div>
+);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import Main from '../components/MainApp';
- 
-export default () => (<Main/>);
 
+export default () => (<Main />);
